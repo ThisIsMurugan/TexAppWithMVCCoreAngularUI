8f11e10b803ae5e243878a68fd7667f971b89ef0
Murugan P <murugan.palaniswamy@yahoo.com>
2020-05-01 15:31:04 +0530

    Angular SubProject

---
+++ TexAngularWithCoreAPI/Models/Ledger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TexAngularWithCoreAPI.Models
+{
+    public class Ledger
+    {
+        public int LedgerId { get; set; }
+        public string LedgerName { get; set; }
+        public string LedgerCode { get; set; }
+    }
+}
