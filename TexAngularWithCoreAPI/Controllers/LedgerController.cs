0b591537420ea4d163db30725751f03612b2bdd9
Murugan P <murugan.palaniswamy@yahoo.com>
2020-05-01 17:31:54 +0530

    Disabled to CamelCase form MVC Core and Enabled the TS latest build

---
+++ TexAngularWithCoreAPI/Controllers/LedgerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TexAngularWithCoreAPI.Models;
+
+namespace TexAngularWithCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LedgerController : ControllerBase
+    {
+        static List<Ledger> ledgers = new List<Ledger>();
+        // GET: api/Ledger
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Ledger/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Ledger
+        [HttpPost]
+        public List<Ledger> Post([FromBody] Ledger ledger)
+        {
+            ledgers.Add(ledger);
+            return ledgers;
+        }
+
+        // PUT: api/Ledger/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- TexAngularWithCoreAPI/Startup.cs
+++ TexAngularWithCoreAPI/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace TexAngularWithCoreAPI
 {
@@ -31,7 +32,9 @@ namespace TexAngularWithCoreAPI
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc()
+                .AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver())
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
