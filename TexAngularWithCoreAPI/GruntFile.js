54ad7c3412df728fe500a74cf869aba6df795c63
Murugan P <murugan.palaniswamy@yahoo.com>
2020-05-03 19:04:13 +0530

    Added Angular SubModule

---
+++ TexAngularWithCoreAPI/GruntFile.js
@@ -0,0 +1,11 @@
+﻿module.exports = function (grunt) {
+    //load the task
+    grunt.loadNpmTasks('grunt-shell');
+    //init config
+    grunt.initConfig({
+        shell: {        
+            command: ["cd C:\\Users\\z003y3xd\\source\\repos\\TexAngularWithCoreAPI\\TexAngularWithCoreAPI\\wwwroot\\TexUI","ng build --force --deploy-url=/TexUI/dist/TexUI/ --watch"].join('&&')
+          }
+    });
+    grunt.registerTask('ngbuildtask', ['shell']);
+}
